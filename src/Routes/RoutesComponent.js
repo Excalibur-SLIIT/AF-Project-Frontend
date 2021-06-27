c47c184639ce872f485c79dfea1ddf0f31a094fa
shevon <shevonkrishmal1998@gmail.com>
2021-06-28 00:41:41 +0530

    add comments

--- src/Routes/RoutesComponent.js
+++ src/Routes/RoutesComponent.js
@@ -17,7 +17,8 @@ class RoutesComponent extends Component {
                     <Route path="/researchersignup" exact component={ResearcherSignUp} />
                     <Route path="/wsconductorsignup" exact component={WSConductorSignUp} />
                     <Route path="/signin" exact component={SignIn} />
-
+                    
+                    {/* ADMIN */}
                     <Route path="/admin/dashboard" exact component={AdminDashBoard} />
 
                 </Switch>
