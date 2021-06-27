cc4f54df662c9dd262651a70cc4d0b86da1aeb35
shevon <shevonkrishmal1998@gmail.com>
2021-06-28 00:39:45 +0530

    edit routes

--- src/Pages/AdminPages/dashboardPage.js
+++ src/Pages/AdminPages/dashboardPage.js
@@ -8,7 +8,7 @@ export default class dashboardPage extends Component {
         return (
             <div>
                  <HeaderModule />
-                <Dashboard />
+                 <Dashboard />
                 <FooterModule />
                 
             </div>
