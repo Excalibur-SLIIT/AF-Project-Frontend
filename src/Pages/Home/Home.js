77bdfed72ff32121b9ba521a47921c2d17d1113c
DIlushaCF <dilushafernando97@gmail.com>
2021-06-27 14:49:15 +0530

    footer draft implemented

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from '../../CommonModules/Header';
+import Footer from '../../CommonModules/Footer';
 
 export class Home extends Component {
     render() {
         return (
             <div>
                 <Header />
+                <Footer />
             </div>
         )
     }
